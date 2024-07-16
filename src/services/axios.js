085954255c620bf3cf11c1124e2657d2698986c2
Michel Souza <91084191+MichelNsouza@users.noreply.github.com>
2024-07-16 14:24:46 -0300

    url api

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/'
-  //baseURL: 'https://api-gerenciador-matriculas.vercel.app/api/api/'
+  //baseURL: 'http://127.0.0.1:8000/api/'
+  baseURL: 'https://api-gerenciador-matriculas.vercel.app/api/api/'
 });
 
 export const getTodosAlunos = () => api.get('alunos');
